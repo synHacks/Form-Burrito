cbe57932451fbe427f3401b8e95938d142a46e0b
Gary Tou <garytou2@gmail.com>
2020-09-01 04:20:22 -0700

    Update config.js for synHacks

--- src/config.js
+++ src/config.js
@@ -1,10 +1,19 @@
 // Create a firebase project. Add a web app to that project and copy the Config here
-const firebaseConfig = {};
+const firebaseConfig = {
+	apiKey: "AIzaSyCJFdvJoIMQuiL6gFHeKJvA_mceip-35g8",
+    	authDomain: "form-burrito.firebaseapp.com",
+   	databaseURL: "https://form-burrito.firebaseio.com",
+   	projectId: "form-burrito",
+	storageBucket: "form-burrito.appspot.com",
+	messagingSenderId: "781659640542",
+    	appId: "1:781659640542:web:96e0619192056a4472a84f",
+   	measurementId: "G-8HP8R9M5M9"
+};
 
 const config = {
 	firebaseConfig: firebaseConfig,
-	homeRedirect: "https://github.com/garytou2/Form-Burrito", //redirects to this URL if you visit the home page
-	entityName: "Form Burrito", //The name of your business, organization, or your own name!
+	homeRedirect: "https://synhacks.org", //redirects to this URL if you visit the home page
+	entityName: "synHacks", //The name of your business, organization, or your own name!
 };
 
 //Also, feel free to change /public/favicon.png to your own logo
